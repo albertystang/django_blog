852408666005dca8eb07fbe093d4c1bd9464b167
albertystang <99115311+albertystang@users.noreply.github.com>
2025-12-18 15:41:15 +0800

    single blog and search

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [    
     path('', views.home, name='home'),
-    path('category/<int:cat_id>/', views.posts_by_category, name='posts_by_category'),    
+    path('category/<int:cat_id>/', views.posts_by_category, name='posts_by_category'),
+    path('<slug:slug>/', views.single_blog, name='single_blog'),
+    path('blogs/search/', views.search, name='search'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
+from django.db.models import Q
 from .models import Category, Blog
+from assignments.models import About
 
 
 def home(request):
     categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published').order_by('-updated_at')
+    try:
+        about = About.objects.get()
+    except:
+        about = None
     context = {
         'categories': categories,
         'featured_posts': featured_posts,
-        'posts': posts
+        'posts': posts,
+        'about': about
     }
     return render(request, 'blogs/home.html', context)
 
@@ -23,3 +30,19 @@ def posts_by_category(request, cat_id):
         return render(request, 'blogs/404.html')    
     context = {'posts': posts, 'category': category}
     return render(request, 'blogs/posts_by_category.html', context)
+
+
+def single_blog(request, slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    return render(request, 'blogs/single_blog.html', {'single_blog': single_blog})
+
+
+def search(request):
+    query = request.GET.get('query')    
+    blogs = Blog.objects.filter(Q(title__icontains=query) | Q(short_description__icontains=query) | Q(blog_body__icontains=query), status='Published')  
+    context = {
+        'blogs': blogs,
+        'keyword': query,
+    }
+    return render(request, 'blogs/search.html', context)
+    
